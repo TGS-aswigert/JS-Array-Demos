2a2c284c9d6b32bf127f7ebf8b5e27a4b2dc78e7
Abigail Swigert <aswigert@teksystems.com>
2024-04-22 17:39:22 -0500

    init with working bookstore and todo app

---
+++ Bookstore/scripts.js
@@ -0,0 +1,88 @@
+import { books } from "./books.js";
+
+const bookList = document.getElementById("book-list");
+
+function renderBookList(bookArray) {
+	clearBookList();
+
+	bookArray.forEach(book => {
+		let bookCard = document.createElement("div");
+		bookCard.className = "book-card";
+
+		let bookImg = document.createElement("img");
+		bookImg.src = book.imageLink;
+		bookImg.alt = book.title + " cover";
+		bookImg.className = "book-cover";
+
+		let textBlock = document.createElement("div");
+		textBlock.className = "text";
+
+		let title = document.createElement("p");
+		title.innerText = book.title.slice(0, 30).concat("...");
+		title.className = "title";
+
+		let author = document.createElement("p");
+		author.innerText = book.author;
+		author.className = "author";
+
+		let price = document.createElement("p");
+		price.innerText = "$24.99";
+		price.className = "price";
+
+		textBlock.appendChild(title);
+		textBlock.appendChild(author);
+		textBlock.appendChild(price);
+
+		bookCard.appendChild(bookImg);
+		bookCard.appendChild(textBlock);
+
+		bookList.appendChild(bookCard);
+	});
+}
+
+function clearBookList() {
+	bookList.innerHTML = "";
+}
+
+function filterShortBooks() {
+	return books.filter(book => {
+		return book.pages <= 300;
+	});
+}
+
+function filterMedBooks() {
+	return books.filter(book => {
+		return book.pages > 300 && book.pages <= 500;
+	});
+}
+
+function filterLongBooks() {
+	return books.filter(book => {
+		return book.pages > 500;
+	});
+}
+
+function sortByNewlyAdded() {
+	return books.reverse();
+}
+
+renderBookList(books);
+
+document.getElementById("short-books").addEventListener("click", () => {
+	renderBookList(filterShortBooks());
+});
+
+document.getElementById("med-books").addEventListener("click", () => {
+	renderBookList(filterMedBooks());
+});
+
+document.getElementById("long-books").addEventListener("click", () => {
+	renderBookList(filterLongBooks());
+});
+
+document.getElementById("new").addEventListener("click", () => {
+	renderBookList(sortByNewlyAdded());
+});
+
+//TODO: add pop to go back a page
+//TODO: add sort
---
+++ Todo List/scripts.js
@@ -0,0 +1,50 @@
+const itemList = document.getElementById("item-list");
+const input = document.getElementById("input-text");
+let todoItems = ["Buy groceries", "Give the dog a bath", "Buy baby shower gift"];
+
+function renderList() {
+    clearList();
+    // todoItems.forEach((elementText, key) => {
+    //     createTodoItem(elementText, key);
+    //  });
+}
+
+function clearList() {
+    itemList.innerHTML = "";
+}
+
+function createTodoItem(itemText, key) {
+	const todoItem = document.createElement("div");
+	todoItem.className = "todo-item";
+
+	const checkbox = document.createElement("input");
+	checkbox.type = "checkbox";
+	checkbox.value = 1;
+	checkbox.name = "item-complete";
+
+	const todoText = document.createElement("p");
+	todoText.innerText = itemText;
+
+	const deleteButton = document.createElement("button");
+	deleteButton.innerText = "Delete";
+	deleteButton.className = "delete-item";
+    deleteButton.onclick = () => removeItem(key);
+
+	todoItem.appendChild(checkbox);
+	todoItem.appendChild(todoText);
+	todoItem.appendChild(deleteButton);
+
+    itemList.appendChild(todoItem);
+}
+
+// function addItem() {
+// 	const inputText = input.value;
+// 	todoItems.push(inputText);
+//     renderList();
+//     input.value = "";
+// }
+
+// function removeItem(key) {
+//     todoItems.splice(key, 1);
+//     renderList();
+// }
